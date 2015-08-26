1b5c18d9913486b05e86deb50e35407227b2fb93
David Goss <dgoss@whiteclarkegroup.com>
2015-08-26 12:05:36 +0100

    Working with the extension to figure out if it's already installed

--- install.js
+++ install.js
@@ -1,13 +1,21 @@
 (function() {
     "use strict";
-    if (chrome && chrome.webstore) {
-        document.getElementById("js-install-button").addEventListener("click", function(e) {
-            e.preventDefault();
-            chrome.webstore.install("", function() {
-                document.body.classList.remove("can-install");
-                document.body.classList.add("is-installed");
-            });
-        });
-        document.body.classList.add("can-install");
+
+    function doCheck() {
+        if (chrome && chrome.webstore) {
+            if (!document.body.classList.contains("is-installed")) {
+                document.getElementById("js-install-button").addEventListener("click", function(e) {
+                    e.preventDefault();
+                    chrome.webstore.install("", function() {
+                        document.body.classList.remove("can-install");
+                        document.body.classList.add("is-installed");
+                    });
+                });
+                document.body.classList.add("can-install");
+            }
+        }
     }
+
+    // give Scrumpy a chance to kick in and tell us it's installed
+    window.setTimeout(doCheck, 1000);
 }());
