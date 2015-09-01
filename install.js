fe9f65ae2fb947661c9948bc6482380dc3886eb1
David Goss <dvdgoss@gmail.com>
2015-09-01 10:05:43 +0100

    Tweaking extension install logic

--- install.js
+++ install.js
@@ -1,15 +1,15 @@
 "use strict";
 (function() {
     if (chrome && chrome.webstore) {
-        if (!document.body.classList.contains("is-installed")) {
+        if (!document.documentElement.classList.contains("is-installed")) {
             document.getElementById("js-install-button").addEventListener("click", function(e) {
                 e.preventDefault();
                 chrome.webstore.install("", function() {
-                    document.body.classList.remove("can-install");
-                    document.body.classList.add("is-installed");
+                    document.documentElement.classList.remove("can-install");
+                    document.documentElement.classList.add("is-installed");
                 });
             });
-            document.body.classList.add("can-install");
+            document.documentElement.classList.add("can-install");
         }
     }
 }());
