a17e9159b4494b6f87930e175679896bdf1b06cb
David Goss <dgoss@whiteclarkegroup.com>
2015-09-01 08:24:05 +0100

    No longer artificially waiting for content script as it should execute first now

--- install.js
+++ install.js
@@ -1,21 +1,15 @@
+"use strict";
 (function() {
-    "use strict";
-
-    function doCheck() {
-        if (chrome && chrome.webstore) {
-            if (!document.body.classList.contains("is-installed")) {
-                document.getElementById("js-install-button").addEventListener("click", function(e) {
-                    e.preventDefault();
-                    chrome.webstore.install("", function() {
-                        document.body.classList.remove("can-install");
-                        document.body.classList.add("is-installed");
-                    });
+    if (chrome && chrome.webstore) {
+        if (!document.body.classList.contains("is-installed")) {
+            document.getElementById("js-install-button").addEventListener("click", function(e) {
+                e.preventDefault();
+                chrome.webstore.install("", function() {
+                    document.body.classList.remove("can-install");
+                    document.body.classList.add("is-installed");
                 });
-                document.body.classList.add("can-install");
-            }
+            });
+            document.body.classList.add("can-install");
         }
     }
-
-    // give Scrumpy a chance to kick in and tell us it's installed
-    window.setTimeout(doCheck, 1000);
 }());
